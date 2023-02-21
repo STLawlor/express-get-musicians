9640c3408c49779d0665502c04a1a541a4462d56
Stacey Lawlor <stacey.tlawlor@outlook.com>
2023-02-21 12:09:09 +0000

    add musicians with id endpoint

--- server.js
+++ server.js
@@ -5,9 +5,14 @@ const { sequelize } = require("./db");
 
 const port = 3000;
 
-app.use("/musicians", async (request, response) => {
-  let musicians = await Musician.findAll();
-  response.json(musicians);
+// app.use("/musicians", async (request, response) => {
+//   let musicians = await Musician.findAll();
+//   response.json(musicians);
+// });
+
+app.use("/musicians/:id", async (request, response) => {
+  let musician = await Musician.findByPk(request.params.id);
+  response.json(musician);
 });
 
 app.listen(port, () => {
