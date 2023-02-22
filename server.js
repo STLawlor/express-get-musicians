e9aacac83f89bf67f86e2dbb34974e2a025515e7
Stacey Lawlor <stacey.tlawlor@outlook.com>
2023-02-22 14:41:45 +0000

    add CRUD endpoints

--- server.js
+++ server.js
@@ -4,22 +4,60 @@ const { Musician } = require("./Musician");
 const { sequelize } = require("./db");
 
 const port = 3000;
+app.use(express.urlencoded({ extended: true }));
 
-// app.use("/musicians", async (request, response) => {
-//   let musicians = await Musician.findAll();
-//   response.json(musicians);
-// });
-
-app.use("/musicians/:id", async (request, response) => {
+app.get("/musicians", async (req, res) => {
   try {
-    let musician = await Musician.findByPk(request.params.id);
+    let musicians = await Musician.findAll();
+    if (musicians) {
+      res.json(musicians);
+    } else {
+      throw new Error("Musicians not found");
+    }
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+app.get("/musicians/:id", async (req, res) => {
+  try {
+    let musician = await Musician.findByPk(req.params.id);
     if (!musician) {
       throw new Error("Musician not found");
     } else {
-      response.json(musician);
-    } 
+      res.json(musician);
+    }
   } catch (err) {
-    response.status(404).send(err.message)
+    res.send(err);
+  }
+});
+
+app.post("/musicians", async (req, res) => {
+  try {
+    await Musician.create(req.body);
+    res.send("Musician created");
+  } catch (err) {
+    res.send(err.message);
+  }
+});
+
+app.put("/musicians/:id", async (req, res) => {
+  try {
+    let musician = await Musician.findByPk(req.params.id);
+    await musician.update(req.body);
+    res.send("Musician updated");
+  } catch (err) {
+    res.send(err.message);
+  }
+});
+
+app.delete("/musicians/:id", async (req, res) => {
+  try {
+    let musician = await Musician.findByPk(req.params.id);
+    musician.destroy();
+    res.send("Musician deleted");
+  } catch (err) {
+    res.send(err.message);
   }
 });
 
